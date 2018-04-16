3d2db8e4cc210686aef7653c00ee79feeee412b3
YasCueLav <yaslavcue@gmail.com>
2018-04-16 14:23:07 -0300

    a

--- Proyecto/Tesis-Asistencias/src/java/Controladores/GestorAlumnos.java
+++ Proyecto/Tesis-Asistencias/src/java/Controladores/GestorAlumnos.java
@@ -5,10 +5,26 @@
  */
 package Controladores;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Yasmin
  */
 public class GestorAlumnos {
+    
+    private Connection conn;
+    
+    public GestorAlumnos (){
+        AccesoDatos ad = new AccesoDatos();
+        try {
+            conn = DriverManager.getConnection(ad.getConn_string(), ad.getUser(), ad.getPass());
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
     
 }
---
+++ Scripts/Insert Condiciones-Cursos.sql
@@ -0,0 +1,22 @@
+----INSERT CONDICIONES----
+INSERT INTO Condiciones
+VALUES ('Aprobación Directa', 0)
+
+INSERT INTO Condiciones
+VALUES ('Promoción de Sistema de Unidades', 0)
+
+INSERT INTO Condiciones
+VALUES ('Regular', 0)
+
+INSERT INTO Condiciones
+VALUES ('Libre por no Cumplimiento', 0)
+
+INSERT INTO Condiciones
+VALUES ('Libre por Ausencias', 0)
+
+----INSERT CURSOS----
+INSERT INTO Cursos
+VALUES ('Integración','1v1',0)
+
+INSERT INTO Cursos
+VALUES ('Integración','1v2',0)
