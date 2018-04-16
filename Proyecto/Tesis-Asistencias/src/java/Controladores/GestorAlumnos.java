c3f8c39628f400207658b61b9b6decfdcc4872af
YasCueLav <yaslavcue@gmail.com>
2018-04-16 18:37:00 -0300

    Casi fin Gestor Alumnos

--- Proyecto/Tesis-Asistencias/src/java/Controladores/GestorAlumnos.java
+++ Proyecto/Tesis-Asistencias/src/java/Controladores/GestorAlumnos.java
@@ -3,6 +3,7 @@ package Controladores;
 import Model.Alumno;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -25,7 +26,7 @@ public class GestorAlumnos {
         }
     }
     
-    public ArrayList<Alumno> ObtenerAlumnos (){
+    public ArrayList<Alumno> obtenerAlumnos (){
         ArrayList<Alumno> lista = new ArrayList<>();
         try {
             Statement stmt = conn.createStatement();
@@ -33,6 +34,7 @@ public class GestorAlumnos {
             while (query.next()){
                 Alumno a = new Alumno();
                 a.setIdAlumno(query.getInt("id_alumno"));
+                a.setLegajo(query.getInt("legajo"));
                 a.setNombre(query.getString("nombre"));
                 a.setApellido(query.getString("apellido"));
                 a.setIdCurso(query.getInt("id_curso"));
@@ -48,4 +50,84 @@ public class GestorAlumnos {
         }
         return lista;
     }
+    
+    public Alumno obtenerAlumno (int id) {
+        Alumno a = new Alumno();
+        try {
+            PreparedStatement stmt = conn.prepareStatement("select * from Alumnos where id_alumno = ?");
+            stmt.setInt(1, id);
+            ResultSet query = stmt.executeQuery();
+            if (query.next()) {
+                a.setIdAlumno(query.getInt("id_alumno"));
+                a.setLegajo(query.getInt("legajo"));
+                a.setNombre(query.getString("nombre"));
+                a.setApellido(query.getString("apellido"));
+                a.setIdCurso(query.getInt("id_curso"));
+                a.setIdCondicion(query.getInt("id_condicion"));
+                a.setGrupo(query.getInt("grupo"));
+            }
+            query.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return a;
+    }
+    //TERMINAR
+    public boolean modificarAlumno (Alumno a) {
+        boolean modifico = true;
+        try {
+            PreparedStatement stmt = conn.prepareStatement("");
+            stmt.setInt(1, a.getIdAlumno());
+            stmt.setInt(2, a.getLegajo());
+            stmt.setString(3, a.getNombre());
+            stmt.setString(4, a.getApellido());
+            stmt.setInt(5, a.getIdCondicion());
+            stmt.setInt(6, a.getGrupo());
+            stmt.executeUpdate();
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            modifico = false;
+        }
+        return modifico;
+    }
+    //TERMINAR
+    public boolean elimniarAlumno (Alumno a, int id) {
+        boolean modifico = true;
+        try {
+            PreparedStatement stmt = conn.prepareStatement("");
+            stmt.setBoolean(1, a.isVisible());
+            stmt.executeUpdate();
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            modifico = false;
+        }
+        return modifico;
+    }
+    //TERMINAR
+    public boolean agregarAlumno (Alumno a) {
+        boolean inserto = true;
+        try {
+            PreparedStatement stmt = conn.prepareStatement("");
+            stmt.setInt(1, a.getIdAlumno());
+            stmt.setInt(2, a.getLegajo());
+            stmt.setString(3, a.getNombre());
+            stmt.setString(4, a.getApellido());
+            stmt.setInt(5, a.getIdCondicion());
+            stmt.setInt(6, a.getGrupo());
+            stmt.executeUpdate();
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            inserto = false;
+        }
+        return inserto;
+    }
+
 }
