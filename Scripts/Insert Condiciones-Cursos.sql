631877cec48cc48b5167838583827d41f022a519
YasCueLav <yaslavcue@gmail.com>
2018-04-19 17:45:55 -0300

    l

--- Scripts/Insert Condiciones-Cursos.sql
+++ Scripts/Insert Condiciones-Cursos.sql
@@ -20,3 +20,41 @@ VALUES ('Integración','1v1',0)
 
 INSERT INTO Cursos
 VALUES ('Integración','1v2',0)
+
+----INSERT TPs----
+INSERT INTO Trabajos_Practicos
+VALUES ('Trabajo Practico 1',0)
+
+INSERT INTO Trabajos_Practicos
+VALUES ('Trabajo Practico 2',0)
+
+INSERT INTO Trabajos_Practicos
+VALUES ('Trabajo Practico 3',0)
+
+INSERT INTO Trabajos_Practicos
+VALUES ('Trabajo Practico 4',0)
+
+INSERT INTO Trabajos_Practicos
+VALUES ('Trabajo Practico 5',0)
+
+INSERT INTO Trabajos_Practicos
+VALUES ('Trabajo Practico Final Integrador',0)
+
+----INSERT Tipo Examen----
+INSERT INTO Tipos_Examenes
+VALUES ('Parcial',0)
+
+INSERT INTO Tipos_Examenes
+VALUES ('Recuperatorio',0)
+
+----INSERT Tipo Examen----
+INSERT INTO Examenes
+VALUES (1,'Siestemas de Unidades',0)
+
+INSERT INTO Examenes
+VALUES (2,'Siestemas de Unidades',0)
+
+select * from Examenes
+
+select e.id_examen , t.tipo_examen , e.examen 
+from Examenes e join Tipos_Examenes t on e.id_tipo_examen = t.id_tipo_examen
