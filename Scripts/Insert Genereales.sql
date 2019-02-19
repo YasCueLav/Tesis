f347d57d417e5dabbcbb5b8d8fd2058dd3cd2e2e
YasCueLav <yaslavcue@gmail.com>
2019-02-19 17:08:00 -0300

    insert vs

---
+++ Scripts/Insert Genereales.sql
@@ -0,0 +1,121 @@
+/*------------------------------*/
+SET LANGUAGE 'Español'
+
+INSERT INTO Alumnos(legajo, apellido, nombre, id_curso, id_condicion , grupo , fecha_ingreso, visible) VALUES (80409,'Aguado', 'Ana Carolina', 2 , 3, null,'01/04/2018',1)
+INSERT INTO Alumnos(legajo, apellido, nombre, id_curso, id_condicion , grupo , fecha_ingreso, visible) VALUES (80692,'Aguero', 'Nemesis Ayelen', 2 , 3, null,'01/04/2018',1)
+INSERT INTO Alumnos(legajo, apellido, nombre, id_curso, id_condicion , grupo , fecha_ingreso, visible) VALUES (78984,'Aldeco Molina', 'Juan Ignacio', 2 , 3, null,'01/04/2018',1)
+INSERT INTO Alumnos(legajo, apellido, nombre, id_curso, id_condicion , grupo , fecha_ingreso, visible) VALUES (81385,'Aliendro', 'Javier Ignacio', 2 , 3, null,'01/04/2018',1)
+--//////////////////////////////////////////////////////
+INSERT INTO Alumnos(legajo, nombre, apellido, id_curso, id_condicion , grupo , fecha_ingreso, visible)VALUES (78932,'Ania Micaela' ,'Acosta Villafañe', 1 , 3, null,'01/04/2018',1)
+INSERT INTO Alumnos(legajo, nombre, apellido, id_curso, id_condicion , grupo , fecha_ingreso, visible)VALUES (78339,'Francisco' ,'Aizpeolea Gerbaudo', 1 , 3, null, '01/04/2018',1)
+INSERT INTO Alumnos(legajo, nombre, apellido, id_curso, id_condicion , grupo , fecha_ingreso, visible)VALUES (75778,'María Victoria' ,'Alamo', 1 , 3, null, '01/04/2018',1)
+INSERT INTO Alumnos(legajo, nombre, apellido, id_curso, id_condicion , grupo , fecha_ingreso, visible)VALUES (79022,' Francisco' ,'Allende Bean', 1 , 3, null, '01/04/2018',1)
+
+/*------------------------------*/
+--6/4/18
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (1,'2018/04/06',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (2,'2018/04/06',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (3,'2018/04/06',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (4,'2018/04/06',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (5,'2018/04/06',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (6,'2018/04/06',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (7,'2018/04/06',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (8,'2018/04/06',1,0,1)
+/*------------------------------*/
+--13/4/18
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (1,'2018/04/13',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (2,'2018/04/13',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (3,'2018/04/13',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (4,'2018/04/13',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (5,'2018/04/13',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (6,'2018/04/13',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (7,'2018/04/13',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (8,'2018/04/13',1,0,1)
+/*------------------------------*/
+--20/4/18
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (1,'2018/04/20',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (2,'2018/04/20',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (3,'2018/04/20',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (4,'2018/04/20',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (5,'2018/04/20',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (6,'2018/04/20',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (7,'2018/04/20',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (8,'2018/04/20',1,0,1)
+/*------------------------------*/
+--27/4/18
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (1,'2018/04/27',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (2,'2018/04/27',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (3,'2018/04/27',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (4,'2018/04/27',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (5,'2018/04/27',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (6,'2018/04/27',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (7,'2018/04/27',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (8,'2018/04/27',1,0,1)
+/*------------------------------*/
+--4/5/18
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (1,'2018/05/04',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (2,'2018/05/04',0,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (3,'2018/05/04',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (4,'2018/05/04',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (5,'2018/05/04',0,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (6,'2018/05/04',0,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (7,'2018/05/04',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (8,'2018/05/04',1,0,1)
+/*------------------------------*/
+--11/5/18 PARCIAL----------- NO CARGADO AUN
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (1,'2018/05/11',1,1,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (2,'2018/05/11',1,1,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (3,'2018/05/11',1,1,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (4,'2018/05/11',1,1,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (5,'2018/05/11',1,1,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (6,'2018/05/11',1,1,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (7,'2018/05/11',1,1,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (8,'2018/05/11',1,1,1)
+/*------------------------------*/
+--18/5/18
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (1,'2018/05/18',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (2,'2018/05/18',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (3,'2018/05/18',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (4,'2018/05/18',0,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (5,'2018/05/18',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (6,'2018/05/18',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (7,'2018/05/18',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (8,'2018/05/18',1,0,1)
+/*------------------------------*/
+--1/6/18
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (1,'2018/06/01',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (2,'2018/06/01',0,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (3,'2018/06/01',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (4,'2018/06/01',0,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (5,'2018/06/01',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (6,'2018/06/01',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (7,'2018/06/01',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (8,'2018/06/01',1,0,1)
+/*------------------------------*/
+--8/6/18
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (1,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (2,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (3,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (4,'2018/06/08',0,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (5,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (6,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (7,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) VALUES (8,'2018/06/08',1,0,1)
+/*------------------------------*/
+--15/6/18----------------
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) 
+VALUES (1,'2018/06/15',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) 
+VALUES (2,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) 
+VALUES (3,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) 
+VALUES (4,'2018/06/08',0,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) 
+VALUES (5,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) 
+VALUES (6,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) 
+VALUES (7,'2018/06/08',1,0,1)
+INSERT INTO Asistencias (id_alumno,fecha_registro,esta_Precente,obligatoria,visible) 
+VALUES (8,'2018/06/08',1,0,1)
